42264949848133487ad318af1ce43ff53df9c2ed
Amiiiiiiiin <90452342+Amiiiiiiiin@users.noreply.github.com>
2022-06-08 10:43:53 +0200

    Add project files.

---
+++ WineCoolerLib/Cooler.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace WineCoolerLib
+{
+    public class Cooler
+    {
+        private int _bottlesInStorage;
+        public int CoolerId { get; set; }
+        public int CapacityOfBottles { get; set; }
+        public int Temp { get; set; }
+
+        public int BottlesInStorage
+        {
+            get => _bottlesInStorage;
+            set
+            {
+                if (value < 0 || value > CapacityOfBottles) throw new ArgumentOutOfRangeException("Value out of range");
+                _bottlesInStorage = value;
+            }
+        }
+
+        public Cooler(int coolerId, int capacityOfBottles, int temp, int bottlesInStorage)
+        {
+            CoolerId = coolerId;
+            CapacityOfBottles = capacityOfBottles;
+            Temp = temp;
+            BottlesInStorage = bottlesInStorage;
+        }
+
+        public Cooler()
+        {
+
+        }
+
+        public bool CoolerIsFull()
+        {
+            if (BottlesInStorage == CapacityOfBottles)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public int AddWine()
+        {
+            if (BottlesInStorage == CapacityOfBottles)
+            {
+                throw new ArgumentOutOfRangeException("Max Capacity");
+            }
+            else
+            {
+                return BottlesInStorage++;
+            }
+        }
+    }
+}
---
+++ WineCoolerLibTests/CoolerTests.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WineCoolerLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WineCoolerLib.Tests
+{
+    [TestClass()]
+    public class CoolerTests
+    {
+        private Cooler cooler;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            cooler = new Cooler(1, 48, 10, 48);
+        }
+
+        [TestMethod()]
+        public void CoolerIsFull()
+        {
+            Cooler cooler2 = new Cooler(2, 48, 10, 47);
+            Assert.AreEqual(true, cooler.CoolerIsFull());
+            Assert.AreEqual(false, cooler2.CoolerIsFull());
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void CoolerIsFullException1()
+        {
+            Cooler cooler3 = new Cooler(3, 48, 10, -1);
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void CoolerIsFullException2()
+        {
+            Cooler cooler4 = new Cooler(4, 48, 10, 49);
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void AddWineTest1()
+        {
+            cooler.AddWine();
+        }
+
+        [TestMethod()]
+        public void AddWineTest2()
+        {
+            Cooler cooler5 = new Cooler(5, 48, 10, 2);
+            cooler5.AddWine();
+            Assert.AreEqual(3, cooler5.AddWine());
+        }
+    }
+}
